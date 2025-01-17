6a446618dd990b289fc75906cf80afba2c18edab
Jeovany Carlos Queiroz <jeovanyc314@gmail.com>
2025-01-17 11:08:26 -0300

    Delete JavaScript directory

--- JavaScript/Minimun.js
+++
@@ -1,8 +0,0 @@
-var min = function (x , y) {
-    if (x < y) {
-        return y
-    } else {
-        return x
-    }
-}
- console.log(min(3 , 4))
--- JavaScript/countBs Function.js
+++
@@ -1,9 +0,0 @@
-function countChar(s , l) {
-    var counter = 0;
-    for (i = 0; i < s.length; i++) {
-        if (s.charAt(i) === l)
-            counter++;
-    }
-    return counter;
-}
-console.log(countChar("kakkerlak", "k"))
